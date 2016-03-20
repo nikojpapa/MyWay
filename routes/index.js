1b00966ba98a661dc3b02c6a39ecdac9d63ebc7b
nikojpapa <nicholas.papadopoulos@comcast.net>
2016-03-20 10:46:19 -0400

    searchQuery function

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,23 @@
+import json
+
 var express = require('express');
 var router = express.Router();
+
+/* Functions */
+function searchQuery(theUrl, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
+}
+
+function searchResults(price) {
+	
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
