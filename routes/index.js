34922f6ab9c9d5adebc601c56082b1ff718afb46
cdchung1 <cchung1@bu.edu>
2016-04-05 14:48:36 -0400

    asynchronous problems

--- routes/index.js
+++ routes/index.js
@@ -12,61 +12,64 @@ var url = 'mongodb://localhost:27017/TestLocalHost';
 /* GET home page. */
 router.get('/', function(req, res, next) {
     var location_field = req.param('location');
-    if (location_field)
-    {
-        // if the location field has been filled
-        // Do a search based on the location
-        var query = "https://partner-api.groupon.com/deals.json?tsToken=US_AFF_0_201236_212556_0&limit=10&division_id=" + location_field;
-        // Using request module to pass results
-        request(query, function(err, resp, body) {
-            var results = JSON.parse(body);
-            if (results.error) {
-                res.render('index', { title: 'MyWay', location: location_field, message: results.error.message});
+    if (location_field) {
+
+        //need to check to see if our search term is already in the database. If so, don't make api call and instead render json object from db
+        MongoClient.connect(url, function (err, db) {
+            if (err) {
+                return console.dir(err);
             }
+
+            var collection = db.collection('test');
+            var searchTerm = location_field.toString();
+
+            console.log('CHECKING FOR A CACHED RESULT ON SEARCH TERM: ' + searchTerm);
+            var previouslyFound = collection.find({'searchTerm': searchTerm});
+            var finalResult = {};
+            if (previouslyFound) {
+                // res.render('index', {title: 'MyWay', location: location_field, resultJSON: previouslyFound['cachedResults']});
+                console.log('PREVIOUS RESULT FOUND');
+                
+                finalResult = previouslyFound['cachedResults'];
+            }
+
             else {
-                MongoClient.connect(url, function(err, db) {
 
-                    if(err) { return console.dir(err); }
+                var query = "https://partner-api.groupon.com/deals.json?tsToken=US_AFF_0_201236_212556_0&limit=10&division_id=" + location_field;
+                // Using request module to pass results
+                request(query, function (err, resp, body) {
+                    var results = JSON.parse(body);
+                    finalResult = results;
 
-                    var collection = db.collection('test');
-                    var doc1 = {'hello':'doc1'};
-                    var doc2 = {'hello':'doc2'};
-                    var searchTerm= location_field.toString();
-                    console.log('SEARCH TERM: ' + searchTerm);
-                    var cachedResults = {'searchTerm': searchTerm, 'cachedResults': results};
-                    // cachedResults[s]= results;
-                    var lotsOfDocs = [{'hello':'doc3'}, {'hello':'doc4'}];
+                    if (results.error) {
+                        res.render('index', {title: 'MyWay', location: location_field, message: results.error.message});
+                    }
 
-                    // collection.insert(cachedResults);
-                    collection.update({'searchTerm':searchTerm}, {$set:{'cachedResults':results}}, {w:1}, function(err, result) {
-                        console.log('ERROR: '+err);
-                        collection.insert(cachedResults, {w:1}, function(err, result) {});
-                    });
+                    else {
+                        MongoClient.connect(url, function (err, db) {
 
-                    // collection.insert(cachedResults, {w:1}, function(err, result) {
-                    //     console.log('Update');
-                    //     console.log('SEARCH TERM: ' + searchTerm);
-                    //     collection.update({'searchTerm':searchTerm}, {$set:{'cachedResults':results}}, {w:1}, function(err, result) {});
-                    // });
+                            if (err) {
+                                return console.dir(err);
+                            }
 
-                     // collection.insert(doc2, {w:1}, function(err, result) {});
-                    //
-                    // collection.insert(lotsOfDocs, {w:1}, function(err, result) {});
+                            var searchTerm = location_field.toString();
+                            console.log('SEARCH TERM: ' + searchTerm);
+                            var cachedResults = {'searchTerm': searchTerm, 'cachedResults': results};
+                            // cachedResults[s]= results;
+                            var lotsOfDocs = [{'hello': 'doc3'}, {'hello': 'doc4'}];
 
-                    // Get an additional db
-                    // db.createCollection('test', {strict:true}, function(err, collection) {});
-            // /*        // console.log('connected');*/
-                    console.log('connected');
-                    // db.close();
+                            // collection.insert(cachedResults);
+                            // collection.update({'searchTerm': searchTerm}, {$set: {'cachedResults': results}}, {w: 1}, function (err, result) {
+                            //     console.log('ERROR: ' + err);
+                            collection.insert(cachedResults);
+                        });
+                    }
                 });
-
-                res.render('index', { title: 'MyWay', location: location_field, resultJSON: results});
             }
 
+            res.render('index', {title: 'MyWay', location: location_field, resultJSON: finalResult});
         });
-    }
-    else
-    {
+    } else {
         // else the location field has not been filled
         res.render('index', { title: 'MyWay'});
     }
