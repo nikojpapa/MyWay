6d8cef6860009df80ea7297272e6340c9c68367e
cdchung1 <cchung1@bu.edu>
2016-04-06 11:48:51 -0400

    Added database query that checks if entered location_id is already in the database. still need to render json object.

--- routes/index.js
+++ routes/index.js
@@ -16,60 +16,69 @@ router.get('/', function(req, res, next) {
 
         //need to check to see if our search term is already in the database. If so, don't make api call and instead render json object from db
         MongoClient.connect(url, function (err, db) {
+
             if (err) {
                 return console.dir(err);
             }
 
-            var collection = db.collection('test');
-            var searchTerm = location_field.toString();
+            var collection = db.collection('new');
+            var searchTermTest = location_field.toString();
 
-            console.log('CHECKING FOR A CACHED RESULT ON SEARCH TERM: ' + searchTerm);
-            var previouslyFound = collection.find({'searchTerm': searchTerm});
-            var finalResult = {};
-            if (previouslyFound) {
-                // res.render('index', {title: 'MyWay', location: location_field, resultJSON: previouslyFound['cachedResults']});
-                console.log('PREVIOUS RESULT FOUND');
-                
-                finalResult = previouslyFound['cachedResults'];
-            }
 
-            else {
+            collection.findOne({'searchTerm': searchTermTest}, function (err, doc) {
+                console.log(doc);
+                var testResult = doc;
+                if (testResult == null) {
+                    console.log("Entry not found in database, performing API call...");
+                }
 
-                var query = "https://partner-api.groupon.com/deals.json?tsToken=US_AFF_0_201236_212556_0&limit=10&division_id=" + location_field;
-                // Using request module to pass results
-                request(query, function (err, resp, body) {
-                    var results = JSON.parse(body);
-                    finalResult = results;
+                if (testResult != null) {
+                    console.log("Entry already found in database");
+                    // res.render('index', {title: 'MyWay', location: location_field, resultJSON: testResult});
 
-                    if (results.error) {
-                        res.render('index', {title: 'MyWay', location: location_field, message: results.error.message});
-                    }
+                }
 
-                    else {
-                        MongoClient.connect(url, function (err, db) {
+            });
 
-                            if (err) {
-                                return console.dir(err);
-                            }
-
-                            var searchTerm = location_field.toString();
-                            console.log('SEARCH TERM: ' + searchTerm);
-                            var cachedResults = {'searchTerm': searchTerm, 'cachedResults': results};
-                            // cachedResults[s]= results;
-                            var lotsOfDocs = [{'hello': 'doc3'}, {'hello': 'doc4'}];
-
-                            // collection.insert(cachedResults);
-                            // collection.update({'searchTerm': searchTerm}, {$set: {'cachedResults': results}}, {w: 1}, function (err, result) {
-                            //     console.log('ERROR: ' + err);
-                            collection.insert(cachedResults);
-                        });
-                    }
-                });
-            }
-
-            res.render('index', {title: 'MyWay', location: location_field, resultJSON: finalResult});
         });
-    } else {
+
+        // if the location field has been filled
+        // Do a search based on the location
+        var query = "https://partner-api.groupon.com/deals.json?tsToken=US_AFF_0_201236_212556_0&limit=10&division_id=" + location_field;
+        // Using request module to pass results
+        request(query, function (err, resp, body) {
+            var results = JSON.parse(body);
+            var finalResults = results;
+            if (results.error) {
+                res.render('index', {title: 'MyWay', location: location_field, message: results.error.message});
+            }
+            else {
+                MongoClient.connect(url, function (err, db) {
+
+                    if (err) {
+                        return console.dir(err);
+                    }
+
+                    var collection = db.collection('new');
+
+                    var searchTerm = location_field.toString();
+                    console.log('SEARCH TERM: ' + searchTerm);
+                    var cachedResults = {'searchTerm': searchTerm, 'cachedResults': results};
+
+
+                    collection.insert(cachedResults);
+
+                    console.log('connected');
+                });
+
+                res.render('index', {title: 'MyWay', location: location_field, resultJSON: finalResults});
+            }
+
+            });
+        }
+
+    else
+    {
         // else the location field has not been filled
         res.render('index', { title: 'MyWay'});
     }
