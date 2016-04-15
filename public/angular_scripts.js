ba30fb725fa08d15332cd716f0c3bf43b891933a
Jia-Yao <jiasherryyao@gmail.com>
2016-04-15 16:48:20 -0400

    removing register and login forms

--- public/angular_scripts.js
+++ public/angular_scripts.js
@@ -1,30 +1,5 @@
 var app = angular.module('myWay', []);
 
-app.directive('username', function($q) {
-    return {
-        require: 'ngModel',
-        link: function(scope, elm, attrs, ctrl) {
-            ctrl.$asyncValidators.username = function(value) {
-                // value is the username input
-                var def = $q.defer();
-
-                // NEED MORE WORK HERE to see if username is already taken
-                /*
-                if () {
-                    // The username is available
-                    def.resolve();
-                } else {
-                    // The username is taken
-                    def.reject();
-                }
-                */
-                scope.formFields.user = value;
-                return def.promise;
-            };
-        }
-    };
-});
-
 app.directive('display', function($compile, $parse) {
     return {
         restrict: 'E',
@@ -41,17 +16,9 @@ app.controller('myWayCtrl', function($scope, $http, $sce) {
     /*
         Initialize scope variables
         - $scope.templates has two fields: nav (for displaying the navigation bar), main (for displaying the main content)
-            - nav can be assigned: navigation.html (if user is not logged in), navigation-user.html (if user is logged in)
             - main can be assigned:
                 homepage.html,
-                login.html,
-                login-success.html,
-                register.html,
-                register-success.html,
                 search.html,
-                profile.html,
-                user-groups.html,
-                logout-success.html
         - $scope.display contains all other html for displaying the page:
             - searchResults
         - $scope.formFields contains all form inputs:
@@ -61,18 +28,13 @@ app.controller('myWayCtrl', function($scope, $http, $sce) {
             - maxPrice
             - startDate
             - endDate
-            - email
-            - user
-            - password
-        - $scope.user contains user information
-            - username
      */
     $scope.templates = {};
     $scope.templates.nav = "./templates/navigation.html";
     $scope.templates.main = "./templates/homepage.html";
     $scope.display = {};
-    $scope.formFields = {};
     $scope.user = {};
+    $scope.formFields = {};
 
     // Define event handler functions
     $scope.toHomepage = function() {
@@ -112,59 +74,6 @@ app.controller('myWayCtrl', function($scope, $http, $sce) {
                     }
                 }
             });
-    };
-
-    $scope.toSignUp = function() {
-        $scope.templates.main = "./templates/register.html";
-    };
-
-    $scope.newUser = function() {
-        // NEED MORE WORK HERE to add user to database
-        var success = true;
-        if (success){
-            $scope.user.username = angular.copy($scope.formFields.user);
-            $scope.templates.nav = "./templates/navigation-user.html";
-            $scope.templates.main = "./templates/register-success.html";
-        }
-        else{
-            $scope.display.errorMessage = "<div class='alert alert-danger'><strong>Registration failed!</strong> Please try again later.</div>";
-            $scope.display.errorMessage = $sce.trustAsHtml($scope.display.errorMessage);
-        }
-    };
-
-    $scope.toLogIn = function() {
-        $scope.templates.main = "./templates/login.html";
-    };
-
-    $scope.loginUser = function() {
-        // NEED MORE WORK HERE to check if email/password is correct
-        var user = "USER";      // Should be replaced with the actual username
-        var success = true;
-        if (success){
-            $scope.user.username = user;
-            $scope.templates.nav = "./templates/navigation-user.html";
-            $scope.templates.main = "./templates/login-success.html";
-        }
-        else{
-            $scope.display.errorMessage = "<div class='alert alert-danger'><strong>Incorrect email/password combination!</strong></div>";
-            $scope.display.errorMessage = $sce.trustAsHtml($scope.display.errorMessage);
-        }
-    };
-    
-    $scope.toEditProfile = function() {
-
-    };
-
-    $scope.toViewMyGroups = function() {
-
-    };
-
-    $scope.toLogOut = function() {
-        $scope.user = {};
-        $scope.display = {};
-        $scope.formFields = {};
-        $scope.templates.nav = "./templates/navigation.html";
-        $scope.templates.main = "./templates/logout-success.html";
     };
 
     $scope.toFilter = function() {
