c7f6414e004681ccd7be6e73a0bead818ad0479a
Jia-Yao <jiasherryyao@gmail.com>
2016-04-20 14:31:04 -0400

    limit results to group deals

--- public/angular_scripts.js
+++ public/angular_scripts.js
@@ -16,18 +16,11 @@ app.controller('myWayCtrl', function($scope, $http, $sce) {
     /*
         Initialize scope variables
         - $scope.templates has two fields: nav (for displaying the navigation bar), main (for displaying the main content)
-            - main can be assigned:
-                homepage.html,
-                search.html,
         - $scope.display contains all other html for displaying the page:
             - searchResults
         - $scope.formFields contains all form inputs:
             - location
             - category
-            - minPrice
-            - maxPrice
-            - startDate
-            - endDate
      */
     $scope.templates = {};
     $scope.templates.nav = "./templates/navigation.html";
@@ -45,7 +38,7 @@ app.controller('myWayCtrl', function($scope, $http, $sce) {
 
     $scope.toSearch = function() {
         $scope.templates.main = "./templates/search.html";
-
+        $scope.formFields.category = undefined;
         $http.get('http://localhost:3000/api', {params: {location: $scope.formFields.location}})
             .then(function(response) {
                 if (response.data.message){
@@ -64,7 +57,9 @@ app.controller('myWayCtrl', function($scope, $http, $sce) {
                             + deal.dealUrl.toString()
                             + "><p>"
                             + deal.title.toString()
-                            + "</p></a></div>";
+                            + "</p></a></div><div><p class='price'>"
+                            + deal.options[0].price.formattedAmount
+                            + "</p></div>";
                         if ($scope.user.username === undefined){
                             $scope.display.searchResults += "<button class='btn btn-info center-block disabled' data-toggle='tooltip' data-placement='right' title='Please login first'><span class='glyphicon glyphicon-plus'></span> Join Group</button></div></div>";
                         }
@@ -77,7 +72,38 @@ app.controller('myWayCtrl', function($scope, $http, $sce) {
     };
 
     $scope.toFilter = function() {
-
+        if ($scope.formFields.category !== undefined){
+            $http.get('http://localhost:3000/api', {params: {location: $scope.formFields.location, category: $scope.formFields.category}})
+                .then(function(response) {
+                    if (response.data.message){
+                        $scope.display.searchResults = "<h3>"+response.data.message.toString()+"</h3>";
+                    }
+                    if (response.data.resultJSON){
+                        var total = response.data.resultJSON.deals.length;
+                        $scope.display.searchResults = "";
+                        for(var i=0;i<total;i++) {
+                            var deal = response.data.resultJSON.deals[i];
+                            $scope.display.searchResults += "<div class='col-sm-6 col-md-4'><div ng-bind-html='display.errorMessage'></div><div class='thumbnail'><a target='_blank' href="
+                                + deal.dealUrl.toString()
+                                + "><img src="
+                                + deal.grid4ImageUrl.toString()
+                                + "></a><div class='caption'><a target='_blank' href="
+                                + deal.dealUrl.toString()
+                                + "><p>"
+                                + deal.title.toString()
+                                + "</p></a></div><div><p class='price'>"
+                                + deal.options[0].price.formattedAmount
+                                + "</p></div>";
+                            if ($scope.user.username === undefined){
+                                $scope.display.searchResults += "<button class='btn btn-info center-block disabled' data-toggle='tooltip' data-placement='right' title='Please login first'><span class='glyphicon glyphicon-plus'></span> Join Group</button></div></div>";
+                            }
+                            else{
+                                $scope.display.searchResults += "<button class='btn btn-info center-block' ng-click=\"toJoin(\'"+ deal.uuid.toString() +"\')\"><span class='glyphicon glyphicon-plus'></span> Join Group</button></div></div>";
+                            }
+                        }
+                    }
+                });
+        }
     };
 
     $scope.toJoin = function(dealuid) {
--- routes/index.js
+++ routes/index.js
@@ -12,70 +12,109 @@ var url = 'mongodb://localhost:27017/TestLocalHost';
 /* api routing */
 router.get('/api', function(req, res, next) {
     var location_field = req.param('location');
-    if (location_field) {
+    var catetory = req.param('category');
 
-        //need to check to see if our search term is already in the database. If so, don't make api call and instead render json object from db
+    if (location_field && catetory){
+        var searchTerm = location_field.toString().replace(/\s+/g, '-').toLowerCase();
+        // Do a search based on the location and category
+        var query =
+            "https://partner-api.groupon.com/deals.json?tsToken=US_AFF_0_201236_212556_0&limit=150&division_id="
+            + searchTerm
+            + "&filters=category:"
+            + catetory.toString().toLowerCase();
+        // Using request module to pass results
+        request(query, function (err, resp, body) {
+            var results = JSON.parse(body);
+            if (results.error) {
+                return res.json({message: results.error.message});
+            }
+            else {
+                // Limit results to only group deals
+                var finalResults = {};
+                finalResults.deals = [];
+                var total = results.deals.length;
+                for(var i=0;i<total;i++) {
+                    var deal = results.deals[i];
+                    var numOptions = deal.options.length;
+                    for(var j=0;j<numOptions;j++) {
+                        var title = deal.options[j].title.toLowerCase();
+                        if ((title.indexOf("for two") > -1) || (title.indexOf("for four") > -1)){
+                            finalResults.deals.push(deal);
+                            break;
+                        }
+                    }
+                }
+                // console.log(finalResults);
+                if (finalResults.deals.length == 0){
+                    return res.json({message: "Deal Not found"});
+                }
+                else {
+                    return res.json({resultJSON: finalResults});
+                }
+            }
+        });
+
+    }
+    else if (location_field) {
+        //need to check to see if our search term is already in the database. If so, don't make api call.
         MongoClient.connect(url, function (err, db) {
-
             if (err) {
                 return console.dir(err);
             }
-
             var collection = db.collection('new');
             var searchTerm = location_field.toString().replace(/\s+/g, '-').toLowerCase();
-            console.log(searchTerm);
 
             collection.findOne({'searchTerm': searchTerm}, function (err, doc) {
                 // console.log(doc); //prints json object for test purposes
                 var testResult = doc;
                 if (testResult == null) {
                     console.log("Entry not found in database, performing API call...");
-                    // if the location field has been filled
                     // Do a search based on the location
-                    var query = "https://partner-api.groupon.com/deals.json?tsToken=US_AFF_0_201236_212556_0&limit=12&division_id=" + searchTerm;
+                    var query = "https://partner-api.groupon.com/deals.json?tsToken=US_AFF_0_201236_212556_0&limit=100&division_id=" + searchTerm;
                     // Using request module to pass results
                     request(query, function (err, resp, body) {
                         var results = JSON.parse(body);
-                        var finalResults = results;
                         if (results.error) {
                             return res.json({message: results.error.message});
                         }
                         else {
-                                console.log('SEARCH TERM: ' + searchTerm);
-                                var cachedResults = {'searchTerm': searchTerm, 'cachedResults': results};
+                            // Limit results to only group deals
+                            var finalResults = {};
+                            finalResults.deals = [];
+                            var total = results.deals.length;
+                            for(var i=0;i<total;i++) {
+                                var deal = results.deals[i];
+                                var numOptions = deal.options.length;
+                                for(var j=0;j<numOptions;j++) {
+                                    var title = deal.options[j].title.toLowerCase();
+                                    if ((title.indexOf("for two") > -1) || (title.indexOf("for four") > -1)){
+                                        finalResults.deals.push(deal);
+                                        break;
+                                    }
+                                }
+                            }
+                            // console.log(finalResults);
+                            if (finalResults.deals.length == 0){
+                                return res.json({message: "Deal Not found"});
+                            }
+                            else {
+                                var cachedResults = {'searchTerm': searchTerm, 'cachedResults': finalResults};
                                 collection.insert(cachedResults);
                                 console.log('connected, new document inserted');
+                                return res.json({resultJSON: finalResults});
+                                return res.json({resultJSON: finalResults});
                             }
-
-                            return res.json({resultJSON: finalResults});
-                        });
-
-
+                        }
+                    });
                 }
 
                 if (testResult != null) {
                     console.log("Entry already found in database");
-                    //begin messing around
-                    // var query = "https://partner-api.groupon.com/deals.json?tsToken=US_AFF_0_201236_212556_0&limit=10&division_id=" + location_field;
-                    // request(query, function (err, resp, body) {
-                    //     var results = JSON.parse(body);
-                    //     var finalResults = results;
-                    //
-                    //
-                    //
-                    //     res.render('index', {title: 'MyWay', location: location_field, resultJSON: finalResults});
-                    // });
-                    //end
-                    // var results = JSON.parse();
                     var finalResults = testResult.cachedResults;
                     return res.json({resultJSON: finalResults});
-
                 }
-
             });
-
         });
-
     }
 });
 
